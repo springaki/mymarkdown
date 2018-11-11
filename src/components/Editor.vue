19494ca8cfa921866894057f14f229df6dd6506e
Aki <aki.onetime@gmail.com>
2018-11-11 13:17:30 +0900

    ログイン画面

---
+++ src/components/Editor.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="editor">
+    <button>エディター画面</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "editor",
+    data() {
+      return {};
+    }
+  };
+</script>
